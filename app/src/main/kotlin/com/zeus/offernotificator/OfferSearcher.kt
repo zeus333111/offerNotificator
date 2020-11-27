8eaf14fdf4ee6eed6180664a933e2aa61b31e692
zeus <zsrm1094@gmail.com>
2020-11-27 09:42:03 -0600

    added multiple search

--- app/src/main/kotlin/com/zeus/offernotificator/OfferSearcher.kt
+++ app/src/main/kotlin/com/zeus/offernotificator/OfferSearcher.kt
@@ -7,18 +7,21 @@ import org.jsoup.Jsoup
 class OfferSearcher {
 
     private val notificator = Notificator()
-    private val url = "https://www.cyberpuerta.mx/Computo-Hardware/Componentes/Tarjetas-de-Video/Tarjeta-de-Video-EVGA-NVIDIA-GeForce-RTX-3080-FTW3-ULTRA-Gaming-10GB-320-bit-GDDR6X-PCI-Express-x16-4-0.html"
+    //private val url = "https://www.cyberpuerta.mx/Computo-Hardware/Componentes/Tarjetas-de-Video/Tarjeta-de-Video-EVGA-NVIDIA-GeForce-RTX-3080-FTW3-ULTRA-Gaming-10GB-320-bit-GDDR6X-PCI-Express-x16-4-0.html"
+    private val url = "https://www.cyberpuerta.mx/Tarjetas-de-Video-NVIDIA-RTX-Serie-30/"
+    //private val url = "https://www.cyberpuerta.mx/NVIDIA-GeForce-RTX-2060/"
 
     fun startDaemon() = runBlocking {
         while (true) {
             val doc = Jsoup.connect(url).get()
-            //val doc = Jsoup.connect("https://www.cyberpuerta.mx/Computo-Hardware/Componentes/Tarjetas-de-Video/Tarjeta-de-Video-EVGA-NVIDIA-GeForce-RTX-3090-FTW3-Ultra-Gaming-24GB-384-bit-GDDR6X-PCI-Express-x16-4-0.html").get()
-            //println(doc.body())
             println("checking....")
-            val button = doc.select(".cartIconPDP")
+            val button = doc.select(".submitButton")
             button.forEach {
-                println(it.toString())
-                notificator.sendNotification("Disponible", url)
+                //println(it.toString())
+                if (it.text().contains("Añadir al carrito")) {
+                    notificator.sendNotification("Disponible", url)
+                    println("un multiple")
+                }
             }
 
             delay(60_000)
